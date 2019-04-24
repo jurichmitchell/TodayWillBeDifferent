f7b2c2e2244b5491b1e6d5c2902c00b481e1fbed
Mitchell Jurich <mdj50790@ucmo.edu>
2019-04-24 15:06:59 -0500

    Initial commit of proof of concept.

---
+++ ADHDProof/Assets/Scripts/LookAtPoints.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPoints : MonoBehaviour
+{
+	public GameObject point1;
+	public GameObject point2;
+	public GameObject point3;
+	public GameObject point4;
+	private GameObject lookTarget; // Where we currently want the camera to be looking
+	private GameObject posTarget; // Where we currently want the camera to be positioned
+
+	public int rotSpeed = 5;
+	public int moveSpeed = 5;
+
+	// Start is called before the first frame update
+	void Start() {
+		lookTarget = gameObject;
+		posTarget = gameObject;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+			lookTarget = point1;
+		else if (Input.GetKeyDown(KeyCode.Alpha2))
+			lookTarget = point2;
+		else if (Input.GetKeyDown(KeyCode.Alpha3))
+			lookTarget = point3;
+		else if (Input.GetKeyDown(KeyCode.Alpha4))
+			lookTarget = point4;
+		else if (Input.GetKeyDown(KeyCode.Keypad1))
+			posTarget = point1;
+		else if (Input.GetKeyDown(KeyCode.Keypad2))
+			posTarget = point2;
+		else if (Input.GetKeyDown(KeyCode.Keypad3))
+			posTarget = point3;
+		else if (Input.GetKeyDown(KeyCode.Keypad4))
+			posTarget = point4;
+
+		/* Do we need to move? */
+		Vector3 targetPosition = posTarget.transform.position;
+		if (gameObject.transform.position != targetPosition)
+			Move();
+
+		/* Do we need to rotate */
+		// If we aren't trying to look at the position we are currently in
+		if (lookTarget.transform.position != gameObject.transform.position) {
+			Quaternion targetRotation = Quaternion.LookRotation(lookTarget.transform.position - gameObject.transform.position, Vector3.up);
+			// If we aren't already looking in the correct direction
+			if (gameObject.transform.rotation != targetRotation)
+				Rotate();
+		}
+	}
+
+	void Rotate() {
+		// The final direction vector we want to look in
+		Vector3 direction = lookTarget.transform.position - gameObject.transform.position;
+		// Get the camera's current rotation and the rotation if it was looking towards the target
+		Quaternion currentRot = gameObject.transform.rotation;
+		Quaternion targetRot = Quaternion.LookRotation(direction, Vector3.up);
+		// Rotate the camera towards the target
+		gameObject.transform.rotation = Quaternion.Slerp(currentRot, targetRot, rotSpeed * Time.deltaTime);
+	}
+
+	void Move() {
+		// Get the camera's current position and the target position
+		Vector3 currentPos = gameObject.transform.position;
+		Vector3 targetPos = posTarget.transform.position;
+
+		// Move the camera towards the target
+		gameObject.transform.position = Vector3.Lerp(currentPos, targetPos, moveSpeed * Time.deltaTime);
+	}
+}
---
+++ ADHDProof/Assets/Scripts/Word.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Word : MonoBehaviour {
+	public bool usePlane = false; // DEBUG
+
+	public Camera cam; // The main camera
+	private GameObject parentWordContainer; // The parent GameObject for all words
+	public Vector3 defaultPosition; // This is the position that the word defaults to (relative to it's word container parent)
+	public Vector3 currentPosition; // This is where the word is currently located (relative to it's word container parent)
+	//public Vector3 lastMousePos;
+	//public Vector3 changeInMouse;
+	bool dragging = false;
+	public float returnSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start() {
+		parentWordContainer = gameObject.transform.parent.gameObject;
+		defaultPosition = gameObject.transform.localPosition;
+		currentPosition = gameObject.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+		// If mouse is not down, we aren't dragging
+		if (!Input.GetMouseButton(0)) {
+			dragging = false;
+		}
+		if (dragging) {
+			if (usePlane)
+				getNewPosPlane();
+			else
+				getNewPos();			
+		}
+		else {
+			// Return the word to it's default position
+			if (currentPosition != defaultPosition) {
+				currentPosition = Vector3.Lerp(currentPosition, defaultPosition, Time.deltaTime * returnSpeed);
+			}
+		}
+
+		// Update this word's position
+		gameObject.transform.localPosition = currentPosition;
+
+		// Rotate this word towards the camera
+		Vector3 towardsCam = cam.transform.position - gameObject.transform.position;
+		Quaternion targetRot = Quaternion.LookRotation(towardsCam, Vector3.up);
+		gameObject.transform.rotation = targetRot;
+	}
+
+	private void OnMouseOver() {
+		if (Input.GetMouseButtonDown(0))
+			dragging = true;
+			//lastMousePos = Input.mousePosition;
+		/*if (Input.GetMouseButton(0)) {
+			//Vector3 currMousePos = Input.mousePosition;
+			//changeInMouse = lastMousePos - currMousePos;
+			//changeInMouse = new Vector3(changeInMouse.x / Screen.width, changeInMouse.y / Screen.height, 0);
+
+
+
+			//currentPosition = gameObject.transform.position - changeInMouse;
+			//lastMousePos = currMousePos;
+
+			Ray mousePos = cam.ScreenPointToRay(Input.mousePosition);
+			currentPosition = mousePos.GetPoint(2);
+			
+		}*/
+	}
+
+	// Calculates the new position of the word keeping the distance of the word from the camera
+	// equal to the distance the parenWordContainer is from the camera. This results in a more
+	// spherical movement of the word around the camera.
+	void getNewPos() {
+		// Return a ray from the camera towards the world space the mouse is located in
+		//		NOTE: the ray's origin is the near plane of the camera, not the camera's position
+		Ray mousePos = cam.ScreenPointToRay(Input.mousePosition);
+
+		// Get the distance from the ray's origin to parentWordContainer
+		float dist = (parentWordContainer.transform.position - mousePos.origin).magnitude;
+		// Get the world position of a point on the ray with this distance from the ray's origin
+		Vector3 worldPoint = mousePos.GetPoint(dist);
+
+		// Translate the world point relative to parentWordContainer's object space
+		currentPosition = parentWordContainer.transform.InverseTransformPoint(worldPoint);
+
+		Debug.DrawRay(mousePos.GetPoint(0), worldPoint - mousePos.GetPoint(0), Color.red); // DEBUG
+	}
+
+	// Calculates the new position of the word by constructing a plane at the position of
+	// the parentWordContainer, then calculating the distance from the camera to this
+	// plane. This allows the distance to vary, so the word will keep a constant z position
+	// resulting in more planar motion.
+	void getNewPosPlane() {
+		// Return a ray from the camera towards the world space the mouse is located in
+		Ray mousePos = cam.ScreenPointToRay(Input.mousePosition);
+
+		// Create a plane at the world position of the parentWordContainer with normal facing the camera
+		Vector3 containerToCam = (parentWordContainer.transform.position - cam.transform.position);
+		Plane distancePlane = new Plane(containerToCam.normalized, parentWordContainer.transform.position);
+
+		// Figure out the distance from the camera to the plane following the mousePos ray
+		float distance = 0;
+		// Plane.Raycast stores the distance from mousePos's ray.origin to the plane in distance variable
+		distancePlane.Raycast(mousePos, out distance);
+
+		// Get the world position of the point where the ray intersects the plane
+		Vector3 worldPoint = mousePos.GetPoint(distance);
+
+		// Translate the world point relative to parentWordContainer's object space
+		currentPosition = parentWordContainer.transform.InverseTransformPoint(worldPoint);
+
+		Debug.DrawRay(mousePos.GetPoint(0), worldPoint - mousePos.GetPoint(0), Color.red); // DEBUG
+	}
+}
